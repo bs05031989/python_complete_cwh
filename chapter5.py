d49d1381f6af32f49a6637dc84caf84a71901225
Bhupender Sharma <sharma05031989@gmail.com>
2022-08-03 13:46:39 +0530

    chapter 5 code and text

---
+++ chapter5.py
@@ -0,0 +1,21 @@
+my_dict = {"Bhupi": "I am a coder",
+"Sam" : "i am a hacker"}
+
+print(my_dict.get("hello"))
+#print(my_dict["hello"])
+
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+
+
+a = set()
+a.add(5)
+a.add(4)
+
+print(a)
+
+b = {8,11,4,5,6,}
+
+print(a.union(b))
+print(a.intersection(b))
