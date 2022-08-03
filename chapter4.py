8fd5588fc96c6a1435619bdd9225e5d7c93af2e6
Bhupender Sharma <sharma05031989@gmail.com>
2022-08-03 12:50:33 +0530

    Cahapter 4 Code & Text

---
+++ chapter4.py
@@ -0,0 +1,52 @@
+"""friends = ["bhupi",23,"dennis"]
+print(friends)
+print(type(friends))
+
+print(friends[0])
+
+l1 = [1,2,65,7,8,9]
+l1.sort()
+print(l1)
+
+l1_sorted = l1.sort()
+print(l1_sorted)
+
+l1.reverse()
+print(l1)
+
+l1.append(45)
+print(l1)
+
+l1.insert(2,456)
+print(l1)
+
+l1.pop(2)
+print(l1)
+
+tuple = (1,2,3,4,1)
+print(tuple[0])
+
+t1 = ()
+print(type(t1))
+
+t2 = (1,)
+print(t2)
+
+t3 = (1)
+print(t3)
+
+print(tuple.count(1))"""
+
+f1 = input("Enter the fruit")
+f2 = input("Enter the fruit")
+
+my_list = [f1,f2]
+print(my_list)
+
+a = [2,4,5,6]
+print(a[0]+a[1]+a[2]+a[3])
+print(sum(a))
+
+a = (7,0,9,0,8)
+print(a.count(0))
+
