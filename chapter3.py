c6bdf8fb346e9c5142fc82d1356e65bd56b7901e
Bhupender Sharma <sharma05031989@gmail.com>
2022-08-03 12:29:19 +0530

    chapter3 code

---
+++ chapter3.py
@@ -0,0 +1,46 @@
+"""greeting = "good morning"
+name = "Bhupender  "
+c = name + greeting
+print(c)
+
+print(name[1:4])
+
+c = c[1::3]
+print(c)
+
+story = "once upon a time i am studying python once"
+print(len(story))
+
+a = story.capitalize
+print(a)
+
+print(story.endswith("bhupi"))
+print(story.count("once"))
+
+print(story.find("upon"))"""
+
+letter = """ Dear <|Name|>,
+You are selected!,
+<|Date|>"""
+
+"""name  = input("Enter your name\n")
+date = input("Enter date\n")
+
+letter = letter.replace("<|Name|>",name)
+letter = letter.replace("<|Date|>",date)
+
+print(letter)"""
+
+string = " This is   a string       hello"
+doublespaces = string.find("  ")
+print(doublespaces)
+
+st = string.replace("  ","")
+print(st)
+
+let= "dear harry, this is a escape character"
+print(let)
+
+let = "dear harry\n this is a escape character"
+print(let)
+
