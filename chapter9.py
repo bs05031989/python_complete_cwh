16e5291fbaee6af535a1b6fa34f79ba34d3fe9de
Bhupender Sharma <sharma05031989@gmail.com>
2022-08-03 18:37:05 +0530

    Chapter9 text

---
+++ chapter9.py
@@ -0,0 +1,23 @@
+"""f = open("chapter9.txt",'r')
+data = f.read()
+print(data)
+f.close()"""
+
+#f = open("chapter9.txt",'w')
+#f.write("I/O Text experimentation")
+#f.close()
+
+
+"""with open ('chapter9.txt','r') as f:
+    a = f.read()
+    print(a) """
+
+"""for i in range(2,21):
+    with open(fr"C:\Users\Jai Dev Dwawdi\Desktop\python_complete_cwh\tables\multiple of {i}" ,'w') as f:
+        for j in range(1,11):
+            f.write(f"{i} x {j} = {i*j}\n")"""
+
+
+
+        
+
