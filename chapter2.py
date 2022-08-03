b2ba9c770f6eb64b5819e2d7f986799bba1919db
Bhupender Sharma <sharma05031989@gmail.com>
2022-08-03 11:33:03 +0530

    chapter2 code

---
+++ chapter2.py
@@ -0,0 +1,20 @@
+a = 34
+b = 5
+print('the sum of the numbers a & b is :',a+b)
+
+print ('the remainder is :', a%b)
+
+"""a = int(input('enter a number'))
+print(type(a))
+b = input('enter a number:')
+print(type(b))"""
+
+print(a<b)
+
+a = int(input('enter a number :'))
+b = int(input('enter a number :'))
+avg = (a+b)/2
+print(avg)
+
+square = a*a
+print(square)
